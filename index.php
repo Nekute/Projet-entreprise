219d2b42f6dd7c301bf925ab7dc6cad3fe452ed9
Arthur <gtab4A@gmail.com>
2023-01-31 11:53:41 +0100

    a

--- index.php
+++ index.php
@@ -12,8 +12,60 @@
     <title>Accueil</title>
 </head>
 <body>
-<header>qsdqsdsqd</header>
+<header>
+    <a href="/index.php"><img src="Logo.png" alt="logo"></a>
 
-<footer></footer>
+    <nav class="navigation">
+    <div id="mySidenav" class="sidenav">
+        <ul class="menu">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#" class="button">Produits</a>
+                <ul class="subMenu">
+                    <a href="">Tout</a>
+                    <a href="">Jouets</a>
+                    <a href="">Arbres à chat</a>
+                    <a href="">Litières</a>
+                    <a href="">Nourriture</a>
+                    <a href="">Accessoires</a>
+                </ul>
+            </li>
+            <li><a href="#">Qui sommes-nous ?</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div></nav>
+
+    <label for="burger" class="burger">
+        <input id="burger" type="checkbox">
+        <span></span>
+        <span></span>
+        <span></span>
+    </label>
+</header>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" viewBox="0 0 429.543 429.543" xml:space="preserve" width="512" height="512">
+<g>
+    <g>
+        <path style="fill:#77BC1F;" d="M279.436,187.909c-3.556,0-7.049,0.268-10.467,0.766c-12.647-20.774-35.504-34.648-61.604-34.648    c-9.735,0-19.015,1.941-27.486,5.44c-13.123-13.733-31.605-22.302-52.1-22.302c-32.845,0-60.538,21.98-69.224,52.027    C25.215,195.52,0,224.797,0,259.978c0,39.803,32.266,72.069,72.069,72.069h207.366c39.803,0,72.069-32.267,72.069-72.069    C351.505,220.176,319.238,187.909,279.436,187.909z"/>
+        <path style="opacity:0.5;fill:#218649;" d="M127.779,137.165c-32.845,0-60.538,21.98-69.224,52.027    C25.215,195.52,0,224.797,0,259.978c0,39.803,32.266,72.069,72.069,72.069h98.906V151.586    C158.938,142.544,143.995,137.165,127.779,137.165z"/>
+    </g>
+</g>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</svg>
+</footer>
 </body>
 </html>
